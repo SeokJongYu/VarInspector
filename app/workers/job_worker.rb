7df5ecc1544f01a20b2c70b49312acc9b6ce2586
Seok Jong Yu <seokjongyu@gmail.com>
2018-02-06 16:21:34 +0900

    adding the job submit function in job_worker #11

--- app/workers/job_worker.rb
+++ app/workers/job_worker.rb
@@ -9,7 +9,7 @@ class JobWorker
       create_data(analysis)
 
       # create slurm controller and lanch
-      # lanch_slurm_job(analysis)
+      lanch_slurm_job(analysis)
       # monitoring analysis job and do post processing
       # poll_job(analysis)
       # post_processing(analysis)
@@ -27,30 +27,25 @@ class JobWorker
 
       hash1 = eval(blood_file1)
       blood_file1_loc = "public/uploads/store/" + hash1[:id]
-      user_file1 = hash1[:metadata][:filename]
-      user_file1.gsub! '-', ''
 
       hash2 = eval(blood_file2)
       blood_file2_loc = "public/uploads/store/" + hash2[:id]
-      user_file2 = hash2[:metadata][:filename]
-      user_file2.gsub! '-', ''
 
       hash3 = eval(brain_file1)
       brain_file1_loc = "public/uploads/store/" + hash3[:id]
-      user_file3 = hash3[:metadata][:filename]
-      user_file3.gsub! '-', ''
 
       hash4 = eval(brain_file2)
       brain_file2_loc = "public/uploads/store/" + hash4[:id]
-      user_file4 = hash4[:metadata][:filename]
-      user_file4.gsub! '-', ''
 
+      @analysis_name_prefix = analysis.name
+      @analysis_name_prefix.gsub! '-', ''
+      @analysis_name_prefix.gsub! ' ', ''
   
       @dir_str = "/tmp/KAIST/" + project.title + "/" + analysis.id.to_s
-      @blood_file1_str = @dir_str + '/' + user_file1 + "-BL_1.fastq.gz"
-      @blood_file2_str = @dir_str + '/' + user_file2 + "-BL_2.fastq.gz"
-      @brain_file1_str = @dir_str + '/' + user_file3 + "-Br_1.fastq.gz"
-      @brain_file2_str = @dir_str + '/' + user_file4 + "-Br_2.fastq.gz"
+      @blood_file1_str = @dir_str + '/' + @analysis_name_prefix + "-BL_1.fastq.gz"
+      @blood_file2_str = @dir_str + '/' + @analysis_name_prefix + "-BL_2.fastq.gz"
+      @brain_file1_str = @dir_str + '/' + @analysis_name_prefix + "-Br_1.fastq.gz"
+      @brain_file2_str = @dir_str + '/' + @analysis_name_prefix + "-Br_2.fastq.gz"
 
       FileUtils::mkdir_p @dir_str
       #copy the file form /public/uploads/ to working dir
@@ -63,45 +58,45 @@ class JobWorker
   
   
     def lanch_slurm_job(analysis)
-      # @b = PBS::Batch.new(
-      #   host: 'manjaro-codegen',
-      #   lib: '/usr/lib',
-      #   bin: '/usr/bin'
-      #   )
-      # @job_id = @b.submit_script(@script_file)
-  
-      # analysis.job_id = @job_id
-      # analysis.status = "submit"
-      # analysis.save
+
+      config = {"adapter" => "slurm", "Jane Doe" => "biocluster3.kisti.re.kr", "conf" => nil, "bin" => "/usr/bin"}
+      @slurm_adptor = OodCore::Job::Factory.build(config)
+
+      submit_script = "runService.sh " + @dir_str +" "+@analysis_name_prefix
+      puts submit_script
+      slurm_id = @slurm_adptor.submit (submit_script)
+
+      analysis.job_id = slurm_id
+      analysis.status = "submit"
+      analysis.save
+
     end
   
     def poll_job(analysis)
-      # job_id = analysis.job_id
-      # status = analysis.status
-      # stat_server = "submit"
-      # until stat_server == "Done"  do
-      #   @stat = @b.get_job(job_id)
-      #   puts @stat.to_yaml
-      #   puts ">>>>>"
-      #   puts job_id
-      #   @stat.each_key {|key| puts key }
-      #   val = @stat[job_id]
-      #   stat_str = val[:job_state]
-      #   if stat_str == "Q" 
-      #     stat_server = "Queue"
-      #   elsif stat_str == "R"
-      #     stat_server = "Running"
-      #   elsif stat_str == "C"
-      #     stat_server = "Done"
-      #   end
+      job_id = analysis.job_id
+      status = analysis.status
+      stat_server = "submit"
+      until stat_server == "Done"  do
+        @stat = @slurm_adptor.status(job_id)
+        puts @stat.to_yaml
+        puts job_id
+        @stat.each_key {|key| puts key }
+        val = @stat[job_id]
+        stat_str = val[:job_state]
+        if stat_str == "Q" 
+          stat_server = "Queue"
+        elsif stat_str == "R"
+          stat_server = "Running"
+        elsif stat_str == "C"
+          stat_server = "Done"
+        end
   
-      #   if stat_server != status
-      #     analysis.status = stat_server
-      #     analysis.save
-      #     sleep 10
-      #   end
-        
-      # end
+        if stat_server != status
+          analysis.status = stat_server
+          analysis.save
+          sleep 10
+        end
+      end
     end
   
     def post_processing(analysis)
